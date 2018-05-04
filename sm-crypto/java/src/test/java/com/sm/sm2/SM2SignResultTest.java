48712e5f20440491a1babd8f747ad4af216ca74b
william <william.sun@pku.edu.cn>
2018-05-04 13:07:18 -0400

    add files

---
+++ sm-crypto/java/src/test/java/com/sm/sm2/SM2SignResultTest.java
@@ -0,0 +1,28 @@
+package com.sm.sm2;
+
+import org.testng.annotations.Test;
+
+import java.math.BigInteger;
+
+import static org.testng.Assert.*;
+
+public class SM2SignResultTest {
+
+    @Test
+    public void parse() throws Exception {
+
+        BigInteger s = new BigInteger(1, "2332233".getBytes());
+        BigInteger r = new BigInteger(1, "555852221455555".getBytes());
+        SM2SignResult result = new SM2SignResult(r, s);
+
+        final String base64 = result.resultToBase64();
+        assertNotNull(base64);
+
+        final SM2SignResult parseResult = SM2SignResult.parse(base64);
+        assertNotNull(parseResult);
+        assertEquals(parseResult.resultToBase64(), base64);
+
+    }
+
+
+}
